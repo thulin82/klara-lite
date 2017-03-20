afffad324dffca7a3c38d8ee53073ecb33f692b6
Markus Tulin <macky_b@hotmail.com>
2017-03-20 20:38:36 +0100

    Added status route

--- config/route/base.php
+++ config/route/base.php
@@ -3,10 +3,12 @@
 $app->router->add("", function () use ($app) {
     $urlHome  = $app->url->create("");
     $urlAbout = $app->url->create("about");
+    $urlStatus = $app->url->create("status");
     $navbar = <<<EOD
 <navbar>
     <a href="$urlHome">Home</a> | 
-    <a href="$urlAbout">About</a>
+    <a href="$urlAbout">About</a> | 
+    <a href="$urlStatus">Status</a>
 </navbar>
 EOD;
 
@@ -26,10 +28,12 @@ EOD;
 $app->router->add("about", function () use ($app) {
     $urlHome  = $app->url->create("");
     $urlAbout = $app->url->create("about");
+    $urlStatus = $app->url->create("status");
     $navbar = <<<EOD
 <navbar>
     <a href="$urlHome">Home</a> | 
-    <a href="$urlAbout">About</a>
+    <a href="$urlAbout">About</a> | 
+    <a href="$urlStatus">Status</a>
 </navbar>
 EOD;
 
@@ -44,4 +48,16 @@ EOD;
 
     $app->response->setBody($body)
               ->send();
+});
+
+$app->router->add("status", function () use ($app) {
+    $data = [
+        "Server" => php_uname(),
+        "PHP version" => phpversion(),
+        "Included files" => count(get_included_files()),
+        "Memory used" => memory_get_peak_usage(true),
+        "Execution time" => microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'],
+    ];
+
+    $app->response->sendJson($data);
 });
