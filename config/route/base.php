3c5c5501e88adea69b3214681839f2a2ac988185
Markus Tulin <macky_b@hotmail.com>
2017-03-20 19:59:17 +0100

    Improved routing

--- config/route/base.php
+++ config/route/base.php
@@ -1,21 +1,45 @@
 <?php
 
-$app->router->add("", function () {
-    echo <<<EOD
+$app->router->add("", function () use ($app) {
+    $urlHome  = $app->url->create("");
+    $urlAbout = $app->url->create("about");
+    $navbar = <<<EOD
+<navbar>
+    <a href="$urlHome">Home</a> | 
+    <a href="$urlAbout">About</a>
+</navbar>
+EOD;
+
+    $body = <<<EOD
 <!doctype html>
 <meta charset="utf-8">
 <title>Home</title>
+$navbar
 <h1>Home</h1>
 <p>This is the homepage.</p>
 EOD;
+
+    echo $body;
 });
 
-$app->router->add("about", function () {
-    echo <<<EOD
+$app->router->add("about", function () use ($app) {
+    $urlHome  = $app->url->create("");
+    $urlAbout = $app->url->create("about");
+    $navbar = <<<EOD
+<navbar>
+    <a href="$urlHome">Home</a> | 
+    <a href="$urlAbout">About</a>
+</navbar>
+EOD;
+
+    $body = <<<EOD
 <!doctype html>
 <meta charset="utf-8">
 <title>About</title>
+$navbar
 <h1>About</h1>
 <p>This is the page about me.</p>
 EOD;
+
+    echo $body;
 });
